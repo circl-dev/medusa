bee2d551ada9ed1bb5753ea8c88dbb2eb04068ff
Ricardo Silva <ricardormsilva93@gmail.com>
2025-03-28 15:15:00 +0000

    added edit sidebar to route

--- packages/admin/dashboard/src/routes/customers/customer-address-edit/components/customer-address-edit/customer-address-edit.tsx
+++ packages/admin/dashboard/src/routes/customers/customer-address-edit/components/customer-address-edit/customer-address-edit.tsx
@@ -2,7 +2,6 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { HttpTypes } from "@medusajs/types"
 import { Button, toast } from "@medusajs/ui"
 import { useForm } from "react-hook-form"
-import { useTranslation } from "react-i18next"
 import * as zod from "zod"
 
 import { RouteDrawer, useRouteModal } from "../../../../../components/modals"
